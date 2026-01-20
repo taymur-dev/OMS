09a2d17ec417e10bc259fe511f635bfb22f728b3
Hamza Amin <hamzaamin104@gmail.com>
2026-01-20 17:42:35 +0500

    front

--- src/Components/TableLayoutComponents/TableTitle.tsx
+++ src/Components/TableLayoutComponents/TableTitle.tsx
@@ -13,12 +13,19 @@ export const TableTitle = ({ tileName, activeFile }: TableTitleProps) => {
 
   return (
     <div className="flex items-center justify-between my-1.5 text-gray-700">
-      <h1 className="text-4xl font-sans font-semibold">{tileName}</h1>
+      {/* <h1 className="text-4xl font-sans font-semibold">{tileName}</h1> */}
+
+      <h1 className="text-2xl sm:text-4xl font-sans font-semibold">
+        {tileName}
+      </h1>
 
       <div className="flex items-center justify-between">
         <div className="flex items-center text-gray-700 hover:text-gray-900 transition">
           <MdOutlineKeyboardBackspace className="text-xl cursor-pointer mt-[0.20rem]" />
-          <Link className="hover:underline font-medium" to={isAdmin === "admin" ? "/" : "/user/dashBoard"}>
+          <Link
+            className="hover:underline font-medium"
+            to={isAdmin === "admin" ? "/" : "/user/dashBoard"}
+          >
             Back to Dashboard
           </Link>
           <span className="text-gray-500 mx-1">/</span>
